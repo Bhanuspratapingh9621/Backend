e8b8e5b8a6bd4f42f9289fe273de86460ea35191
firstFSDclass <bpsingh5957@gmail.com>
2025-02-07 10:17:51 +0530

    maine aisi change kiya kuch

--- BACKEND/filesystem/file1.js
+++ BACKEND/filesystem/file1.js
@@ -1,3 +1,8 @@
 const fs = require('fs');
 const data = fs.readFileSync("./data.txt","utf-8");
-console.log(data.toString());
+console.log(data);
+if (data.match("H")){
+    console.log(" H is Found");
+    const data = data.repalce("H","ABES");
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
---
+++ BACKEND/filesystem/filecallback.js
@@ -0,0 +1,12 @@
+const { error } = require("console")
+
+const read = ()=>{
+    const fs = require("fs")
+    fs.readFile("./data.txt","utf-8",(error,data)=>{
+        if (error){
+            console.errror("Error reading file",data);
+        }
+    });
+}
+read();
+console.log("complete me first");
